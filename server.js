afaaf11a6429c7a35721d7a18faa49ab0d7e0035
jcepedaf <jcepedaf@gmail.com>
2021-11-09 21:54:48 +0100

    agrego inClass BD week-3 exercise 1

---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "cyf_hotels",
+  password: "123456",
+  port: 5432,
+});
+
+app.get("/hotels", function (req, res) {
+  pool
+    .query("SELECT * FROM hotels")
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.post("/hotels", function (req, res) {
+  const newHotelName = req.body.name;
+  const newHotelRooms = req.body.rooms;
+  const newHotelPostcode = req.body.postcode;
+
+  if (!Number.isInteger(newHotelRooms) || newHotelRooms <= 0) {
+    return res
+      .status(400)
+      .send("The number of rooms should be a positive integer.");
+  }
+
+  pool
+    .query("SELECT * FROM hotels WHERE name=$1", [newHotelName])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return res
+          .status(400)
+          .send("An hotel with the same name already exists!");
+      } else {
+        const query =
+          "INSERT INTO hotels (name, rooms, postcode) VALUES ($1, $2, $3)";
+        pool
+          .query(query, [newHotelName, newHotelRooms, newHotelPostcode])
+          .then(() => res.send("Hotel created!"))
+          .catch((e) => console.error(e));
+      }
+    });
+});
+
+app.get("/customers", function (req, res) {
+    pool
+      .query("SELECT * FROM customers")
+      .then((result) => res.json(result.rows))
+      .catch((e) => console.error(e));
+});
+
+app.post("/customers", function (req, res) {
+    const newCustomerName = req.body.name;
+    const newCustomerEmail = req.body.email;
+    const newCustomerAddress = req.body.address;
+    const newCustomerCity = req.body.city;
+    const newCustomerPostcode = req.body.postcode;
+    const newCustomerCountry = req.body.country;
+
+pool
+    .query("SELECT * FROM customers WHERE name=$1", [newCustomerName])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return res
+          .status(400)
+          .send("A customers with the same name already exists!");
+      } else {
+        const query =
+        "INSERT INTO customers (name, email, address, city, postcode, country) VALUES ($1, $2, $3, $4, $5, $6)";
+        pool
+          .query(query, [newCustomerName, newCustomerEmail, newCustomerAddress, newCustomerCity, newCustomerPostcode, newCustomerCountry])
+          .then(() => res.send("Customer created!"))
+          .catch((e) => console.error(e));
+      }
+    });
+});
+
+
+app.listen(3000, function () {
+    console.log("Server is listening on port 3000. Ready to accept requests!");
+});

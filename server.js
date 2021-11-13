72ed1020114ba01256780794f18f1c58bbc2422d
Jesus Cepeda <jcepedaf@gmail.com>
2021-11-13 14:16:38 +0100

    agrego exercise 2 de inclass week3

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const pool = new Pool({
   user: "postgres",
   host: "localhost",
   database: "cyf_hotels",
-  password: "123456",
+  password: "195319",
   port: 5432,
 });
 
@@ -49,21 +49,21 @@ app.post("/hotels", function (req, res) {
 });
 
 app.get("/customers", function (req, res) {
-    pool
-      .query("SELECT * FROM customers")
-      .then((result) => res.json(result.rows))
-      .catch((e) => console.error(e));
+  pool
+    .query("SELECT * FROM customers")
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
 });
 
 app.post("/customers", function (req, res) {
-    const newCustomerName = req.body.name;
-    const newCustomerEmail = req.body.email;
-    const newCustomerAddress = req.body.address;
-    const newCustomerCity = req.body.city;
-    const newCustomerPostcode = req.body.postcode;
-    const newCustomerCountry = req.body.country;
+  const newCustomerName = req.body.name;
+  const newCustomerEmail = req.body.email;
+  const newCustomerAddress = req.body.address;
+  const newCustomerCity = req.body.city;
+  const newCustomerPostcode = req.body.postcode;
+  const newCustomerCountry = req.body.country;
 
-pool
+  pool
     .query("SELECT * FROM customers WHERE name=$1", [newCustomerName])
     .then((result) => {
       if (result.rows.length > 0) {
@@ -72,16 +72,56 @@ pool
           .send("A customers with the same name already exists!");
       } else {
         const query =
-        "INSERT INTO customers (name, email, address, city, postcode, country) VALUES ($1, $2, $3, $4, $5, $6)";
+          "INSERT INTO customers (name, email, address, city, postcode, country) VALUES ($1, $2, $3, $4, $5, $6)";
         pool
-          .query(query, [newCustomerName, newCustomerEmail, newCustomerAddress, newCustomerCity, newCustomerPostcode, newCustomerCountry])
+          .query(query, [
+            newCustomerName,
+            newCustomerEmail,
+            newCustomerAddress,
+            newCustomerCity,
+            newCustomerPostcode,
+            newCustomerCountry,
+          ])
           .then(() => res.send("Customer created!"))
           .catch((e) => console.error(e));
       }
     });
 });
 
+app.get("/hotels/:hotelId", function (req, res) {
+  const hotelId = req.params.hotelId;
+
+  pool
+    .query("SELECT * FROM hotels WHERE id=$1", [hotelId])
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.get("/customers/:customerId", function (req, res) {
+  const customerId = req.params.customerId;
+
+  pool
+    .query("SELECT * FROM customers WHERE id=$1", [customerId])
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.get("/customers-order", function (req, res) {
+  pool
+    .query("SELECT * FROM customers order by name asc")
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.get("/bookings/:customerId", function (req, res) {
+  const customerId = req.params.customerId;
+
+  pool
+    .query("select bookings.checkin_date,bookings.nights,hotels.name,hotels.postcode from  bookings join hotels on bookings.hotel_id=hotels.id and bookings.customer_id=$1;", [customerId])
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
 
 app.listen(3000, function () {
-    console.log("Server is listening on port 3000. Ready to accept requests!");
+  console.log("Server is listening on port 3000. Ready to accept requests!");
 });
